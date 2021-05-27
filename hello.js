4244a3fe0c3d5305088f0f01a9f459a64dcc6601
racheltest02 <>
2021-05-27 02:51:07 +0000

    trying to see what happends when two window are open

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	return "Hello jhfkhjgj,jhkahfljahasdk.  dsfsdhg!";
+	return "Hello jhhfljahasdk.  dsfsdhg!";
 }
  
 module.exports = hello;
