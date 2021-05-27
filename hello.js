047f5aea92150ffd62977001749536a3a9730e8c
racheltest02 <>
2021-05-27 02:47:24 +0000

    more trial and error

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	return "Hello jhfkhjgjhg!";
+	return "Hello jhfkhjgj,jhkahfljahasdk.  dsfsdhg!";
 }
  
 module.exports = hello;
